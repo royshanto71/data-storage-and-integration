de15c826e18fc25c0a767156df90ae0d3ce02008
Roy Shanto <122017004+royshanto71@users.noreply.github.com>
2025-09-26 02:47:01 +0600

    update 6 files

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import "./App.css";
 import { Suspense } from "react";
 import Bottles from "./Components/Bottles/Bottles";
 
-const bottlesPromise = fetch("./bottles.json").then((response) => response.json());
+const bottlesPromise = fetch("./bottles.json").then((response) =>
+  response.json()
+);
 
 function App() {
   return (
     <>
+      <h1 class="text-7xl font-bold text-center m-7">Hello world!</h1>
       <h1 style={{ textAlign: "center" }}>Best Water Bottles</h1>
       <Suspense fallback={<h2>Loading...</h2>}>
         <Bottles bottlesPromise={bottlesPromise} />
